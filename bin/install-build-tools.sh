14c2e0cbebc565035bd6861aaa39c4d9e4a5e78c
kthompso <KThompso@users.noreply.github.com>
2018-09-08 15:56:43 -0400

    Move curl commands to wget

--- bin/install-build-tools.sh
+++ bin/install-build-tools.sh
@@ -2,8 +2,8 @@ set -x
 set -e
 
 if [ ! -e build_tools ]; then
-    if [ ! $(command -v curl) ]; then
-        apt-get update && apt-get -y install curl
+    if [ ! $(command -v wget) ]; then
+        apt-get update && apt-get -y install wget
     fi
 
     if [ ! $(command -v unzip) ]; then
@@ -11,11 +11,11 @@ if [ ! -e build_tools ]; then
     fi
 
     mkdir build_tools
-    curl -O 'https://force-cli.heroku.com/releases/v0.24.3/linux-amd64/force'
+    wget 'https://force-cli.heroku.com/releases/v0.24.3/linux-amd64/force'
     chmod +x ./force
     mv force build_tools/force
 
-    curl -O 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.7.0/pmd-bin-6.7.0.zip'
+    wget 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.7.0/pmd-bin-6.7.0.zip'
     unzip 'pmd-bin-6.7.0.zip' && rm 'pmd-bin-6.7.0.zip'
     mv 'pmd-bin-6.7.0' build_tools/pmd
 fi
