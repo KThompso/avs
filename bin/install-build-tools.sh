6aed5b87b3cbb6ba492a54b2bf8cd704492c0d00
kthompso <KThompso@users.noreply.github.com>
2018-09-08 15:41:06 -0400

    Add pmd static analyzer

--- bin/install-build-tools.sh
+++ bin/install-build-tools.sh
@@ -5,8 +5,17 @@ if [ ! -e build_tools ]; then
     if [ ! $(command -v curl) ]; then
         apt-get update && apt-get -y install curl
     fi
+
+    if [ ! $(command -v unzip) ]; then
+        apt-get update && apt-get -y install unzip 
+    fi
+
     mkdir build_tools
     curl -O 'https://force-cli.heroku.com/releases/v0.24.3/linux-amd64/force'
     chmod +x ./force
     mv force build_tools/force
+
+    curl -O 'https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.7.0/pmd-bin-6.7.0.zip'
+    unzip 'pmd-bin-6.7.0.zip' && rm 'pmd-bin-6.7.0.zip'
+    mv 'pmd-bin-6.7.0' build_tools/pmd
 fi
